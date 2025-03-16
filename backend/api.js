33b22b95f71c5541d08facf05d57d5d4e412d2c4
Nguyen Hai Long <longnh.it07@gmail.com>
2025-03-16 18:05:48 +0700

    soft delete update + add constrain when update, insert events

--- backend/api.js
+++ backend/api.js
@@ -9,21 +9,24 @@ const port = 3000;
 // the base has the following schema:
 // CREATE TABLE role (
 //     role_id INT AUTO_INCREMENT PRIMARY KEY ,
-//     role_name VARCHAR(256) NOT NULL
+//     role_name VARCHAR(256) NOT NULL,
+//     is_delete BOOL default  FALSE
 // );
 // CREATE TABLE user (
 //     user_id INT AUTO_INCREMENT PRIMARY KEY ,
 //     username VARCHAR(256) NOT NULL,
 //     password VARCHAR(512) NOT NULL,
-//     user_role_id INT NOT NULL REFERENCES role(role_id)
+//     user_role_id INT NOT NULL REFERENCES role(role_id),
+//     is_delete BOOL default  FALSE
 // );
 // CREATE TABLE events (
 //     event_id INT AUTO_INCREMENT PRIMARY KEY ,
 //     event_name TEXT NOT NULL,
 //     event_time DATETIME NOT NULL,
-//     created_date TIME NOT NULL ,
+//     created_date DATETIME DEFAULT NOW(),
 //     event_place TEXT NOT NULL,
-//     created_user_id INT REFERENCES user(user_id)
+//     created_user_id INT REFERENCES user(user_id),
+//     is_delete BOOL default  FALSE
 // );
 
 // create a connection to the database
@@ -85,7 +88,17 @@ api.get('/events/:id', (req, res) => {
 });
 
 // create API for adding a new events to the events table with all the fields
+// the request body is a object that has following property:
+//  event_name, event_time, event_place, created_user_id
 api.post('/events', (req, res) => {
+
+    // check if the request body has fields that are not allow to insert
+    // then return the error message
+    if (req.body.event_id || req.body.created_date || req.body.is_delete) {
+        res.json({ message: 'The request body contains fields that are not allow to insert' });
+        return;
+    }
+
     connection.query('INSERT INTO events SET ?', req.body, (err, rows) => {
         if (err) {
             console.log('Error in query');
@@ -95,8 +108,34 @@ api.post('/events', (req, res) => {
     });
 });
 
+
 // create API for updating an existing events in the events table with specific event_id
+// only allow update the following fields: event_name, event_time, event_place
+// the request body is a object that has following property:
+//  event_name, event_time, event_place
+// only allow update event that is not deleted
+
 api.put('/events/:id', (req, res) => {
+
+    //check if the event is deleted
+    connection.query('SELECT * FROM events WHERE event_id = ?', [req.params.id], (err, rows) => {
+        if (err) {
+            console.log('Error in query');
+            return;
+        }
+        if (rows[0].is_delete) {
+            res.json({ message: 'Event is deleted' });
+            return;
+        }
+    });
+
+    // check if the request body has the fields that are not allowed to update
+    // then return the error message
+    if (req.body.event_id || req.body.created_date || req.body.created_user_id || req.body.is_delete) {
+        res.json({ message: 'The request body contains fields that are not allow to update' });
+        return;
+    }
+
     connection.query('UPDATE events SET ? WHERE event_id = ?', [req.body, req.params.id], (err, rows) => {
         if (err) {
             console.log('Error in query');
@@ -107,8 +146,9 @@ api.put('/events/:id', (req, res) => {
 });
 
 // create API for deleting an existing events in the events table with specific event_id
+// the delete process is a soft delete, the is_delete field will be set to TRUE
 api.delete('/events/:id', (req, res) => {
-    connection.query('DELETE FROM events WHERE event_id = ?', [req.params.id], (err, rows) => {
+    connection.query('UPDATE events SET is_delete = TRUE WHERE event_id = ?', [req.params.id], (err, rows) => {
         if (err) {
             console.log('Error in query');
             return;
@@ -117,3 +157,57 @@ api.delete('/events/:id', (req, res) => {
     });
 });
 
+// create API for retrieve data from role table
+api.get('/role', (req, res) => {
+    connection.query('SELECT * FROM role', (err, rows) => {
+        if (err) {
+            console.log('Error in query');
+            return;
+        }
+        res.json(rows);
+    });
+});
+
+// create API for retrieve data from user table
+api.get('/user', (req, res) => {
+    connection.query('SELECT * FROM user', (err, rows) => {
+        if (err) {
+            console.log('Error in query');
+            return;
+        }
+        res.json(rows);
+    });
+});
+
+// create API for insert a new user to the user table
+api.post('/user', (req, res) => {
+    connection.query('INSERT INTO user SET ?', req.body, (err, rows) => {
+        if (err) {
+            console.log('Error in query');
+            return;
+        }
+        res.json(rows);
+    });
+});
+
+// create API for update an existing user in the user table with specific user_id
+api.put('/user/:id', (req, res) => {
+    connection.query('UPDATE user SET ? WHERE user_id = ?', [req.body, req.params.id], (err, rows) => {
+        if (err) {
+            console.log('Error in query');
+            return;
+        }
+        res.json(rows);
+    });
+});
+
+// create API for delete an existing user in the user table with specific user_id
+api.delete('/user/:id', (req, res) => {
+    connection.query('DELETE FROM user WHERE user_id = ?', [req.params.id], (err, rows) => {
+        if (err) {
+            console.log('Error in query');
+            return;
+        }
+        res.json(rows);
+    });
+});
--- scripts/database.sql
+++ scripts/database.sql
@@ -6,14 +6,16 @@ use schedule;
 
 CREATE TABLE role (
     role_id INT AUTO_INCREMENT PRIMARY KEY ,
-    role_name VARCHAR(256) NOT NULL
+    role_name VARCHAR(256) NOT NULL,
+    is_delete BOOL default  FALSE
 );
 
 CREATE TABLE user (
     user_id INT AUTO_INCREMENT PRIMARY KEY ,
     username VARCHAR(256) NOT NULL,
     password VARCHAR(512) NOT NULL,
-    user_role_id INT NOT NULL REFERENCES role(role_id)
+    user_role_id INT NOT NULL REFERENCES role(role_id),
+    is_delete BOOL default  FALSE
 );
 
 
@@ -23,7 +25,8 @@ CREATE TABLE events (
     event_time DATETIME NOT NULL,
     created_date DATETIME DEFAULT NOW(),
     event_place TEXT NOT NULL,
-    created_user_id INT REFERENCES user(user_id)
+    created_user_id INT REFERENCES user(user_id),
+    is_delete BOOL default  FALSE
 );
 
 
